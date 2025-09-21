3ad9da8d150507a359e073d2e647b6c6d879cb0e
matteo.mullace <matteo.mullace@esternibisp.com>
2025-09-21 19:49:56 +0200

    first commit

---
+++ xme-salute-ai/src/main/java/com/agentai/xme_salute_ai/XmeSaluteAiApplication.java
@@ -0,0 +1,13 @@
+package com.agentai.xme_salute_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmeSaluteAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmeSaluteAiApplication.class, args);
+	}
+
+}
---
+++ xme-salute-ai/src/test/java/com/agentai/xme_salute_ai/XmeSaluteAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agentai.xme_salute_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmeSaluteAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
