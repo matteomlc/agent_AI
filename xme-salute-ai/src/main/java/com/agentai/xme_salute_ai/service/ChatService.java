5a5734eb69937004a9369d9f98f966c24da85689
matteo.mullace <matteo.mullace@esternibisp.com>
2025-09-24 17:07:38 +0200

    feat: added service for streaming and no-streaming call

---
+++ xme-salute-ai/src/main/java/com/agentai/xme_salute_ai/service/ChatService.java
@@ -0,0 +1,31 @@
+package com.agentai.xme_salute_ai.service;
+
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.model.StreamingChatModel;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class ChatService {
+
+    private final ChatModel chatModel;
+
+    public ChatService(ChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    public String ask(String input) {
+        Prompt prompt = new Prompt(new UserMessage(input));
+        ChatResponse resp = chatModel.call(prompt);
+        return resp.getResults().get(0).getOutput().getText();
+    }
+
+    public Flux<String> askStreaming(String input) {
+        Prompt prompt = new Prompt(new UserMessage(input));
+        return this.chatModel.stream(prompt)
+                .map(cr ->cr.getResults().get(0).getOutput().getText());
+    }
+}
